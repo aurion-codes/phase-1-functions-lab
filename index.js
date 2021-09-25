97b9ffa5e88b62ffec1f6b876fe7428d2d67e71d
AurionHoward <aurionhoward@gmail.com>
2021-09-25 11:01:37 -0400

    commit tree 35710c1eb1af2e7a2845ea7ba649b72436d1c44f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){   //Function that takes in a (pickup) location param
+        return Math.abs(blocks - 42);                                           // math here that return a block
+};
+
+
+function distanceFromHqInFeet(start,end){
+       return  (distanceFromHqInBlocks(start,end) * 264);
+};
+
+function distanceTravelledInFeet(start,end){
+    return Math.abs(start - end) * 264
+};
